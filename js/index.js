da230602d76af98706995be3644dc5e8e2595701
komar3 <komar3@uic.edu>
2022-09-04 14:05:12 -0400

    aha

--- js/index.js
+++ js/index.js
@@ -1,22 +1,24 @@
 async function moveCircle() {
-    var circle = document.getElementById("circle");
+    check = document.getElementById('anim').checked
+    var circle = document.getElementById("circle")
     var cx = circle.getAttribute("cx");
     // console.log(typeof cx)
-    cx = parseInt(cx);
-    while(true){
+    cx = parseInt(cx)
+    if(check){
         if(cx > 950) cx = 25
         cx = cx + 10
-        await new Promise(resolve => setTimeout(resolve, 100));
-        circle.setAttribute("cx", cx);
+        await new Promise(resolve => setTimeout(resolve, 100))
+        circle.setAttribute("cx", cx)
     }
 };
 
 async function moveRect() {
-    var rect = document.getElementById("rect");
+    check = document.getElementById('anim').checked
+    var rect = document.getElementById("rect")
     var x = rect.getAttribute("x");
-    console.log(typeof x)
+    // console.log(typeof x)
     x = parseInt(x);
-    while(true){
+    if(check){
         if(x > 970) x = 10
         x = x + 10
         // console.log(x)
@@ -26,15 +28,20 @@ async function moveRect() {
 };
 
 async function moveEllipse() {
-    var elip = document.getElementById("ellipse");
+    check = document.getElementById('anim').checked
+    var elip = document.getElementById("ellipse")
     var cx = elip.getAttribute("cx");
     cx = parseInt(cx);
-    while(true){
+    if(check){
         // console.log(cx)
         if(cx > 930) cx = 70
         cx = cx + 10
         // console.log(x)
-        await new Promise(resolve => setTimeout(resolve, 70));
-        elip.setAttribute("cx", cx);
+        await new Promise(resolve => setTimeout(resolve, 70))
+        elip.setAttribute("cx", cx)
     }
 };
+
+setInterval(moveCircle, 0);
+setInterval(moveRect, 0);
+setInterval(moveEllipse, 0);
