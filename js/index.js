f72777591f31e4a60060b7e9f03a7682a216a4b2
komar3 <komar3@uic.edu>
2022-09-06 17:24:24 -0400

    aha

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
+function colorChange() {
+    var rect = document.getElementById("rect")
+    var fill = rect.getAttribute("fill");
+
+    if(fill == "purple") rect.setAttribute("fill", "orange")
+    else rect.setAttribute("fill", "purple")
+}
+
+function positionChange() {
+    var circle = document.getElementById("circle")
+    var cx = circle.getAttribute("cx");
+
+    cx = cx + 1
+    circle.setAttribute("cx", cx)
+}
+
+function disappear() {
+    var ellipse = document.getElementById("ellipse")
+    var rx = ellipse.getAttribute("rx");
+    var ry = ellipse.getAttribute("ry");
+
+    ellipse.setAttribute("rx", 0)
+    ellipse.setAttribute("ry", 0)
+}
+
 async function moveCircle() {
     check = document.getElementById('anim').checked
     var circle = document.getElementById("circle")
@@ -7,7 +32,7 @@ async function moveCircle() {
     if(check){
         if(cx > 950) cx = 25
         cx = cx + 10
-        await new Promise(resolve => setTimeout(resolve, 100))
+        // await new Promise(resolve => setTimeout(resolve, 400))
         circle.setAttribute("cx", cx)
     }
 };
@@ -22,7 +47,7 @@ async function moveRect() {
         if(x > 970) x = 10
         x = x + 10
         // console.log(x)
-        await new Promise(resolve => setTimeout(resolve, 50));
+        // await new Promise(resolve => setTimeout(resolve, 500));
         rect.setAttribute("x", x);
     }
 };
@@ -37,11 +62,11 @@ async function moveEllipse() {
         if(cx > 930) cx = 70
         cx = cx + 10
         // console.log(x)
-        await new Promise(resolve => setTimeout(resolve, 70))
+        // await new Promise(resolve => setTimeout(resolve, 70))
         elip.setAttribute("cx", cx)
     }
 };
 
-setInterval(moveCircle, 0);
-setInterval(moveRect, 0);
-setInterval(moveEllipse, 0);
+setInterval(moveCircle, 100);
+setInterval(moveRect, 100);
+setInterval(moveEllipse, 100);
